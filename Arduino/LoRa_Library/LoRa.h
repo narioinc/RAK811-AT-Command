41d736a1c851d10f0b8ccd14742c0c2ce3ea14a9
RAK <xiaocheng.cao@rakwireless.com>
2017-05-09 10:05:12 +0800

    Arduino Library

---
+++ Arduino/LoRa_Library/LoRa.h
@@ -0,0 +1,159 @@
+#include<SoftwareSerial.h>
+#define TXpin 1   // Set the virtual serial port pins
+#define RXpin 0
+SoftwareSerial mySerial(RXpin,TXpin);    // Declare a virtual serial port
+char buffer[128];
+
+void cleanbuf()
+{
+  memset(buffer,0,sizeof(buffer));
+}
+
+int rk_getVersion(void)
+{
+ // This function is used to get the module version
+  char* offest;
+ mySerial.write("at+version\r\n"); 
+  delay(100);
+ while (mySerial.available()>0) {
+  delay(100);
+  int numdata = mySerial.available();  
+  mySerial.readBytes(buffer,numdata);
+  //Serial.println(buffer); 
+  if (0==strncmp(buffer,"OK",2)){
+      offest = strstr(buffer,"\r\n");
+      offest[0]= 0;
+      Serial.print("rk get version:");
+      Serial.println(&buffer[2]);
+      cleanbuf();
+      return 0;
+   } else {
+      return -1;
+   }
+ }
+}
+
+int rk_getBand(void)
+{
+  //This function is used to get the frequency band of the module
+  char* offest;
+  mySerial.write("at+band\r\n");    
+  delay(100);
+  while (mySerial.available()>0) {
+  delay(100);
+  int numdata = mySerial.available();  
+  mySerial.readBytes(buffer,numdata);
+  //Serial.println(buffer); 
+  if (0==strncmp(buffer,"OK",2)){
+      offest = strstr(buffer,"\r\n");
+      offest[0]= 0;
+      Serial.print("rk get band:");
+      Serial.println(&buffer[2]);
+      //strcpy(vers,&buffer[2]);
+      cleanbuf();
+      return 0;
+   } else {
+      return -1;
+   }
+ }
+}
+
+void rk_setConfigOTAA(char* devEUI, char* appEUI, char* appKEY)
+{
+  // This function is used to set the necessary parameters for module OTAA
+  char Str[128] = {};
+  sprintf(Str,"at+set_config=dev_eui:%s&app_eui:%s&app_key:%s\r\n",devEUI,appEUI,appKEY);
+  
+  Serial.print(Str);
+  mySerial.write(Str); 
+  delay(500);
+  while (mySerial.available()>0) {
+  delay(100);
+  int numdata = mySerial.available();  
+  mySerial.readBytes(buffer,numdata);
+  Serial.println(&buffer[0]); 
+  cleanbuf();
+ }  
+}
+
+void rk_setConfigABP(char* devADDR, char* nwksKEY, char* appsKEY)
+{
+  // This function is used to set the required parameters for module ABP
+  char Str[128] = {};
+  sprintf(Str,"at+set_config=dev_addr:%s&nwks_key:%s&apps_key:%s\r\n",devADDR,nwksKEY,appsKEY);
+  
+  Serial.print(Str);
+  mySerial.write(Str); 
+  delay(500);
+  while (mySerial.available()>0) {
+  delay(100);
+  int numdata = mySerial.available();  
+  mySerial.readBytes(buffer,numdata);
+  Serial.println(&buffer[0]); 
+  cleanbuf();
+ }  
+}
+
+void rk_joinLoraNetwork(int mode)
+{
+  // This function is used to join the network
+  char Str[20] = {};
+  if (mode == 0)
+  sprintf(Str,"at+join=otaa\r\n");
+  else if (mode == 1)
+  sprintf(Str,"at+join=abp\r\n");
+      
+  Serial.print(Str);
+  mySerial.write(Str);
+  delay(500);
+  while(mySerial.available()>0) {
+    delay(100);
+    int numdata = mySerial.available();  
+    mySerial.readBytes(buffer,numdata);
+    Serial.println(&buffer[0]); 
+    cleanbuf();
+  }  
+   
+  delay(8000);   // Wait for the result to be returned
+  while(mySerial.available()>0) {
+    delay(1000);
+    int numdata = mySerial.available();  
+    mySerial.readBytes(buffer,numdata);
+    Serial.println(&buffer[0]); 
+    cleanbuf();  
+  }
+}
+
+int rk_sendConfirmUplinkData(int port, char* data)
+{
+  // This function is used to send the data to be verified
+  char Str[128] = {};
+  sprintf(Str,"at+send=1,%d,%s\r\n",port,data);
+  Serial.print(Str);
+  mySerial.write(Str);    
+  delay(500);
+  while(mySerial.available()>0) {
+    delay(200);
+    int numdata = mySerial.available();  
+    mySerial.readBytes(buffer,numdata);
+    Serial.println(&buffer[0]);
+    cleanbuf(); 
+  }     
+}
+
+int rk_sendUnConfirmUplinkData(int port, char* data)
+{
+  // This function is used to transmit data need not check
+  char Str[128] = {};
+  sprintf(Str,"at+send=0,%d,%s\r\n",port,data);
+  Serial.print(Str);
+  mySerial.write(Str);    
+  delay(500);
+  while(mySerial.available()>0) {
+    delay(200);
+    int numdata = mySerial.available();  
+    mySerial.readBytes(buffer,numdata);
+    Serial.println(&buffer[0]); 
+    cleanbuf();
+  }     
+}
---
+++ Arduino/LoRa_Library/LoRa_Library.ino
@@ -0,0 +1,79 @@
+#include "LoRa.h"
+
+#define OTAA  0
+#define ABP   1
+
+#define JOIN_MODE OTAA   // set network mode, OTAA or ABP.
+#if JOIN_MODE == OTAA
+char* DevEui = "60C5A8FFFE000001";
+char* AppEui = "70B3D57EF00047C0";
+char* AppKey = "5D833B4696D5E01E2F8DC880E30BA5FE";
+#else JOIN_MODE == ABP
+char* NwkSKey = "3432567afde4525e7890cfea234a5821";
+char* AppSKey = "a48adfc393a0de458319236537a11d90";
+char* DevAddr = "00112233";
+#endif
+int port = 2;
+char* data = "112233445566778899";
+
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(9600);
+ while(Serial.read()>= 0){}
+ while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+ Serial.println("Hello, world?");       
+ mySerial.begin(115200);     
+ mySerial.listen();
+ //while(mySerial.read()>= 0){}
+
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int ret;
+
+while(mySerial.read()>= 0){
+  Serial.println(mySerial.read());
+  }
+
+ret = rk_getVersion();   // get module version 
+if (ret !=0) {
+   Serial.println("Get version failed");
+}
+delay(1000);
+while(mySerial.read()>= 0){}
+
+ret = rk_getBand();    // grt module band
+if (ret !=0) {
+   Serial.println("Failed to get the band");
+  }
+delay(1000);
+while(mySerial.read()>= 0){}
+
+#if JOIN_MODE == OTAA
+rk_setConfigOTAA(DevEui,AppEui,AppKey);
+delay(1000);
+while(mySerial.read()>= 0){}
+#else if JOIN_MODE == ABP
+rk_setConfigABP(DevAddr,NwkSKey,AppSKey);
+delay(1000);
+while(mySerial.read()>= 0){}
+#endif
+
+rk_joinLoraNetwork(JOIN_MODE);   // join Network
+delay(1000);
+while(mySerial.read()>= 0){}
+
+ret = rk_sendConfirmUplinkData(port,data);  // Send the data to be verified 
+delay(1000);
+while(mySerial.read()>= 0){}
+
+ret = rk_sendUnConfirmUplinkData(port,data);   // Send data that does not need to be verified
+delay(1000);
+while(mySerial.read()>= 0){}
+}
+
+
