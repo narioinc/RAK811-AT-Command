8b27a5b3891852c59d40c5b64f00bf1050b9dad7
RAK <xiaocheng.cao@rakwireless.com>
2017-05-16 14:57:06 +0800

    RAK 811 Arduino Library

---
+++ Arduino/RAK811/examples/JoinNetworkABP/JoinNetworkABP.ino
@@ -0,0 +1,65 @@
+
+
+#include "RAK811.h"
+#include "SoftwareSerial.h"
+#define WORK_MODE LoRaWAN   //  LoRaWAN or LoRaP2P
+#define JOIN_MODE ABP    //  OTAA or ABP
+#if JOIN_MODE == OTAA
+String DevEui = "60C5A8FFFE000001";
+String AppEui = "70B3D57EF00047C0";
+String AppKey = "5D833B4696D5E01E2F8DC880E30BA5FE";
+#else JOIN_MODE == ABP
+String NwkSKey = "3432567afde4525e7890cfea234a5821";
+String AppSKey = "a48adfc393a0de458319236537a11d90";
+String DevAddr = "00112233";
+#endif
+#define TXpin 1   // Set the virtual serial port pins
+#define RXpin 0
+SoftwareSerial mySerial(RXpin,TXpin);    // Declare a virtual serial port
+char* buffer = "72616B776972656C657373";
+
+RAK811 RAKLoRa(mySerial);
+
+
+
+void setup() {
+ Serial.begin(38400);
+ while(Serial.read()>= 0) {}
+ while(!Serial);
+ Serial.println("StartUP");
+
+ mySerial.begin(9600);
+
+}
+
+void loop() {
+ 
+ Serial.println(RAKLoRa.rk_getVersion());
+ delay(500);
+ Serial.println(RAKLoRa.rk_getBand());
+ delay(500);
+ if(RAKLoRa.rk_setWorkingMode(WORK_MODE))
+ {
+  Serial.println("you set Working mode is OK!");
+  if (RAKLoRa.rk_initABP(DevAddr,NwkSKey,AppSKey))
+    {
+      Serial.println("you init ABP parameter is OK!");
+      if (RAKLoRa.rk_joinLoRaNetwork(JOIN_MODE))
+       {
+           Serial.println("you join Network is OK!");
+           mySerial.setTimeout(8000);
+           String ver = mySerial.readStringUntil('\n');
+           Serial.println(ver);
+           mySerial.setTimeout(2000);
+           if (ver == STATUS_JOINED_SUCCESS)
+           {
+            while (RAKLoRa.rk_sendData(1,1,buffer))
+            {
+              Serial.println(RAKLoRa.rk_recvData());
+            }
+           }
+        }
+     }
+ }
+ delay(500);
+}
---
+++ Arduino/RAK811/examples/LoRaP2PPingPongTest/LoRaP2PPingPongTest.ino
@@ -0,0 +1,87 @@
+
+
+#include "RAK811.h"
+#include "SoftwareSerial.h"
+#define WORK_MODE LoRaP2P  //  LoRaWAN or LoRaP2P
+#define CODE  PING  // PING or PONG 
+#define TXpin 1
+#define RXpin 0
+SoftwareSerial mySerial(RXpin,TXpin);
+char* buffer = "72616B776972656C657373";
+String FREQ = "860000000";  // frequency     range:(860000000 - 1020000000)
+
+RAK811 RAKLoRa(mySerial);
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(38400);
+  while(Serial.read()>= 0) {}
+  while(!Serial);
+  Serial.println("StartUP");
+
+  mySerial.begin(9600);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(RAKLoRa.rk_getVersion());
+  delay(500);
+  Serial.println(RAKLoRa.rk_getBand());
+  delay(500);
+  if (RAKLoRa.rk_setWorkingMode(WORK_MODE))
+  {
+    Serial.println("you set Working mode is OK!");
+    if (RAKLoRa.rk_initP2P(FREQ,7,0,1,8,14))
+    {
+      Serial.println("you init P2P parameter is OK!");
+      while (1)
+      {
+        #if  CODE == PING
+         if (RAKLoRa.rk_sendP2PData(1,"10",buffer))
+         {
+          String ver = RAKLoRa.rk_recvData();
+          //Serial.println(ver);
+          if (ver == STATUS_P2PTX_COMPLETE)
+          {
+            Serial.println("send success");
+            if (RAKLoRa.rk_recvP2PData(1))
+            {
+              Serial.println("open recv");
+              Serial.println(RAKLoRa.rk_recvData());
+            }
+          }
+          else
+          {
+            Serial.println(ver);
+            delay(2000);
+          }
+         }
+        #else if CODE == PONG
+        if (RAKLoRa.rk_recvP2PData(1))
+         {
+          Serial.println("open recv");
+          Serial.println(RAKLoRa.rk_recvData());
+          if (RAKLoRa.rk_sendP2PData(1,"10",buffer))
+          {
+            String ver = RAKLoRa.rk_recvData();
+            if (ver == STATUS_P2PTX_COMPLETE)
+            {
+              Serial.println("send success");
+            }
+            else
+            {
+              Serial.println(ver);
+              delay(2000);
+            }
+          }
+         }
+        #endif
+      }
+    }
+  }
+
+}
